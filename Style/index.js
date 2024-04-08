444a15666ad31a48ac1190b89277729732729daf
Baquinha <153133119+Baquinha@users.noreply.github.com>
2024-04-08 11:10:16 +0100

    added axios - still trying to fix key

--- Style/index.js
+++ Style/index.js
@@ -1,8 +1,20 @@
+function updateWeatherData(response) {
+  console.log(response);
+}
+
+function searchCity(city) {
+  let apiKey = "7aaa22bfd4o06a5b0f963ta6d1b6fff3";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  axios.get(apiUrl).then(updateWeatherData);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
