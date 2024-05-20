0513967954e85278d5ea22e7e3a2af4ddee4f029
Baquinha <153133119+Baquinha@users.noreply.github.com>
2024-05-20 10:49:51 +0100

    Fixed the API

--- Style/index.js
+++ Style/index.js
@@ -1,16 +1,16 @@
-function updateWeatherData(response) {
+function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
   cityElement.innerHTML = response.data.city;
+
   temperatureElement.innerHTML = Math.round(temperature);
 }
-
 function searchCity(city) {
   let apiKey = "7aaa22bfd4o06a5b0f963ta6d1b6fff3";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(updateWeatherData);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
