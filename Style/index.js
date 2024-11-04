cc668dc4e13bf4752f544ae62d1bc3692e69e2cb
Baquinha <153133119+Baquinha@users.noreply.github.com>
2024-11-04 11:14:00 +0000

    Added forecast

--- Style/index.js
+++ Style/index.js
@@ -51,7 +51,31 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weahter-forecast-day">
+            <div class="weather-forecast-date">${day}</div>
+            <div class="weather-forecast-icon">&#x2600;</div>
+            <div class="weahter-forecast-temperatures">
+              <div class="weahter-forecast-temperature">
+                <strong>15º</strong>
+              </div>
+              <div class="weahter-forecast-temperature">9º</div>
+            </div>
+          </div>
+`;
+  });
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Porto");
+
+displayForecast();
